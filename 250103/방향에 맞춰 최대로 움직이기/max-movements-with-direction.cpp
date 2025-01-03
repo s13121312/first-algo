3762e21350c4045ce23071c13b49abfff1b85100
강정민 <wjdals980424@naver.com>
2025-01-03 17:17:37 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250103/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
+++ 250103/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
@@ -6,7 +6,6 @@ int dy[] = {0,1,1,1,0,-1,-1,-1};
 int n, r, c;
 int arr[5][5];
 int dir[5][5];
-void print(int t[5][5]);
 int ans = 0;
 
 void input(){
@@ -28,13 +27,13 @@ void input(){
     r--;c--;
 
 }
-vector<pair<int, int> > next_pos;
 
 void backtracking(int cur, int x, int y){
-    
+    int d = dir[x][y];
+
     for(int j=1;j<n;j++){
-        int nx = x + dx[dir[x][y]]*j;
-        int ny = y + dy[dir[x][y]]*j;
+        int nx = x + dx[d]*j;
+        int ny = y + dy[d]*j;
         if(nx < 0 || nx >= n || ny<0 || ny>=n || arr[nx][ny]<= arr[x][y])continue;
         //cout << "nx: " << nx << ", ny: " << ny << '\n';
         cur = cur+1;
@@ -47,25 +46,14 @@ void backtracking(int cur, int x, int y){
 
 void pro(){
     
-    
     backtracking(0, r, c);
-
-
+    cout << ans;
 }
 
-void print(int t[5][5]){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout << t[i][j] << ' ';
-        }
-        cout << '\n';
-    }
-}
 int main() {
     // Please write your code here.
     input();
     pro();
-    cout << ans;
 
     return 0;
 }
