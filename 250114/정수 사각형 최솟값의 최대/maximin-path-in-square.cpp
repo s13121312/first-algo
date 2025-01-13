0119298418c772a01c4061a6c19e3ec14f25cc38
강정민 <wjdals980424@naver.com>
2025-01-14 02:01:45 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250114/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int ans =0;
+int arr[105][105];
+int dp[105][105];
+
+void input(){
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin >> arr[i][j];
+        }
+    }
+}
+
+void pro(){
+    dp[0][0] = arr[0][0];
+
+    for(int i=1;i<n;i++){
+        dp[i][0] = min(dp[i-1][0], arr[i][0]);
+    }
+
+    for(int i=1;i<n;i++){
+        dp[0][i] = min(dp[0][i-1], arr[0][i]);
+    }
+
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<n;j++){
+            int tmp = max(min(dp[i-1][j], arr[i][j]), min(dp[i][j-1], arr[i][j]));
+            ans = max(ans, tmp);
+            //cout << ans << '\n';
+        }
+    }
+    
+    cout << ans;
+}
+
+
+int main() {
+    // Please write your code here.
+    input();
+    pro();
+
+    return 0;
+}
