bafbe42920f5e8b5f1fc0216095273930a734dfb
강정민 <wjdals980424@naver.com>
2025-01-15 01:54:50 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250115/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int n;
+int dp[1005];
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    
+    dp[1]=1;
+    dp[2] = 2;
+
+    for(int i=3;i<=n;i++){
+        dp[i] = (dp[i-1] + dp[i-2])%10007;
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
