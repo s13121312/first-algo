c8baf5af2dda10e8958fa3ab7d0564e8545296cf
강정민 <wjdals980424@naver.com>
2025-01-25 01:40:29 +0900

    Title: 그래프 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250125/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> p[1005];
+bool visited[1005];
+int n, m;
+
+void input(){
+    cin >> n >> m;
+    for(int i=0;i<m;i++){
+        int x, y;
+        cin >> x >> y;
+        p[x].push_back(y);
+        p[y].push_back(x);
+    }
+}
+int cnt = 0;
+
+void pro(int vertex){
+    for(int i=0;i<p[vertex].size();i++){
+        if(!visited[p[vertex][i]]){
+            visited[p[vertex][i]]= true;
+            cnt++;
+            pro(p[vertex][i]);
+        }
+
+    }
+}
+
+int main() {
+    // Please write your code here.
+    input();
+    pro(1);
+    cout << cnt-1;
+
+    return 0;
+}
